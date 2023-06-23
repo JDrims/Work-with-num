1d60affa936b5b7911e7b090867ab2132e1b8f60
JDrims <dimas.asus@mail.ru>
2023-06-24 02:25:46 +0600

    initional

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        List<Integer> result = new ArrayList<>();
+        for (int num : intList) {
+            if (num <= 0) continue;
+            if (num % 2 == 0) result.add(num);
+        }
+        Collections.sort(result);
+        System.out.println(result);
+    }
+}
